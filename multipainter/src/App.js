053ba3107509e6649435358c80e6cb58c536022c
Aidan-Trujillo <ajt494@nau.edu>
2024-03-21 15:44:48 -0700

    More stuff

--- multipainter/src/App.js
+++ multipainter/src/App.js
@@ -4,27 +4,25 @@ import './App.css';
 function renderHTMLHeaders() {
   return(
     <>
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title> Multi Pixel </title>
-    <link rel="stylesheet" href="mp.css" />
-    <h1 id="brush"> Current Brush Color Loading</h1>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title> Multi Pixel </title>
+      <link rel="stylesheet" href="mp.css" />
+      <h1 id="brush"> Current Brush Color Loading</h1>
     </>
   );
 }
 
 function App() {
+  renderHTMLHeaders();
+
   return (
     <>
       <renderHTMLHeaders/>
-      {/* Flex Holder for Palette + Board*/}
       <div id="holder">
-        {/* Flex palette for colors*/}
         <div id="palette">
         </div>
-        {/* Board for colors, can be resized*/}
         <div id="board">
-          {/*check to set which board*/}
           <creativeBoard/>
         </div>
       </div>
