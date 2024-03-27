df52467ff296ee8a2fea2ca7d38b01cbf5e572b0
Matthew Gardner <meg546@nau.edu>
2024-03-27 13:44:27 -0700

    Changed the nav bar a little bit, made it bigger, cleaned up the code, and changed the text color

--- multiPainter/src/NavBar/NavBar.js
+++ multiPainter/src/NavBar/NavBar.js
@@ -6,29 +6,27 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <div className="header">
-            {/* Left */}
-            <div className="thing1">
-                <div className="thing3">
-                <img src={logo} className="logo" />
+                {/* Left */}
+                <div className="logoPosition">
+                    <img src={logo} className="logo" />
                 </div>
+            {/* Middle */}
+            <div className="NavBar">
+                <ul>
+                    <li><a href="/Home">Home</a>
+                    </li>
+                    <li><a href="/create">Free Draw</a>
+                    </li>
+                    <li><a href="/Paint">Paint</a>
+                    </li>
+                    <li><a href="/WorkShop">WorkShop</a>
+                    </li>
+                    <li><a href="">Portfolio</a>
+                    </li>
+                    <li><a href="/Signin">Sign-In</a>
+                    </li>
+                </ul>
             </div>
-        {/* Middle */}
-        <div>
-            <ul>
-                <li><a href="/Home">Home</a>
-                </li>
-                <li><a href="/create">Free Draw</a>
-                </li>
-                <li><a href="/Paint">Paint</a>
-                </li>
-                <li><a href="/WorkShop">WorkShop</a>
-                </li>
-                <li><a href="">Portfolio</a>
-                </li>
-                <li><a href="/Signin">Sign-In</a>
-                </li>
-            </ul>
-        </div>
         </div>
     );
 };
