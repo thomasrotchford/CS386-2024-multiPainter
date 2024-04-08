74951f5dbb6936d3dd8a16e9f112df8e1dce3eb5
Aidan-Trujillo <ajt494@nau.edu>
2024-04-07 17:07:44 -0700

    Making obvious necessary changes to this for D5

--- multipainter/src/Community/Community.js
+++ multipainter/src/Community/Community.js
@@ -52,9 +52,9 @@ function Community() {
         {/* Container for text posts */}
         <div className='text-post-container'>
           {/* Text post */}
-          <TextPost title="i honestly love pooping so much." body="I'm sitting here pooping away at the end of a long day before bed. Sitting here browsing the web and watching random stuff online is like meditation and therapy for me. The outside world kind of just disappears for a bit and my mind just quiets down." />
-          <TextPost title="My wife of 24 years just farted in front of me for the first time ever." body="It's never happened before. She's huge on manners and would never discuss anything like farting or pooping, let alone do it in front of someone. I have heard her fart in her sleep before and she would get embarrassed if I mentioned it, so I usually shut up about it. I mean, it's just farts. But we've been quarantined with our 4 kids for weeks now and it's... it's a lot. A few hours ago we were sitting on the couch, drinking our coffee and she just let it out. There was a moment of silence followed by a look of defeat in her eyes and a simple sorry.I'm so happy. It's just a fart, it's weird but it was such a liberating, humorous moment for the two of us and it made my day." />
-          <TextPost title="Post Title" body="poopy" />
+          <TextPost title="I honestly love walking so much." body="I'm sitting here walking away at the end of a long day before bed. Sitting here browsing the web and watching random stuff online is like meditation and therapy for me. The outside world kind of just disappears for a bit and my mind just quiets down." />
+          <TextPost title="I love community." body="I have always really enjoined the community that I can experience on this onlin platform that adds a level of creativity that helps me express myself." />
+          <TextPost title="Post Title" body="Test" />
         </div>
 
         <div className='images-container'>
