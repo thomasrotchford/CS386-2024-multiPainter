e5aada42b31b3a3f4c60f2a2bb0d76b650a386b7
Thomas Andrew Rotchford <thomasrotchford@icloud.com>
2024-04-10 19:50:13 -0700

    Danger database"

---
+++ multipainter/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "CS3862024multiPainter": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
---
+++ multipainter/src/graphql/subscriptions.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTemplates = /* GraphQL */ `
+  subscription OnCreateTemplates(
+    $filter: ModelSubscriptionTemplatesFilterInput
+  ) {
+    onCreateTemplates(filter: $filter) {
+      id
+      timeCreated
+      numGrid
+      colorGrid
+      artName
+      creator
+      creationMessage
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateTemplates = /* GraphQL */ `
+  subscription OnUpdateTemplates(
+    $filter: ModelSubscriptionTemplatesFilterInput
+  ) {
+    onUpdateTemplates(filter: $filter) {
+      id
+      timeCreated
+      numGrid
+      colorGrid
+      artName
+      creator
+      creationMessage
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteTemplates = /* GraphQL */ `
+  subscription OnDeleteTemplates(
+    $filter: ModelSubscriptionTemplatesFilterInput
+  ) {
+    onDeleteTemplates(filter: $filter) {
+      id
+      timeCreated
+      numGrid
+      colorGrid
+      artName
+      creator
+      creationMessage
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
