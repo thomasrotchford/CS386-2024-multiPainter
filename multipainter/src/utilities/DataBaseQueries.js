a16f7ba05d8df90749e936348d8a8d7343b1b695
Matthew Gardner <meg546@nau.edu>
2024-04-27 17:33:48 -0700

    created board template class and implented it into homepage

--- multipainter/src/utilities/DataBaseQueries.js
+++ multipainter/src/utilities/DataBaseQueries.js
@@ -1,19 +1,33 @@
 
-
-import { generateClient } from "aws-amplify/api"; // imports a function that creates a driver for the DB
-                                                    // this allows us to run commands on the database essentially with the client object
-import { getTemplates, listTemplates } from '../graphql/queries';      // this imports a pre-defined query
+import { generateClient } from "aws-amplify/api";
+import { getTemplates, listTemplates } from '../graphql/queries';
 import { createTemplates } from '../graphql/mutations';
-import config from "../aws-exports.js"; // this imports our configuration file, (actual file should not be
-// uploaded to the database "aws-exports.js")
+import config from "../aws-exports.js";
+import { BoardTemplate } from './TemplateClass.js';
+import { Amplify } from 'aws-amplify';
 
-import { Amplify } from 'aws-amplify';  // imports Amplify functions needed to start connection
-// configures the set up with an imported config file
 Amplify.configure(config);
-// generates a client object that allows us to run query scripts and actually mutate
-// and read the data base
 const client = generateClient();
 
+export async function queryTemplates(){
+
+  const response = await client.graphql({
+      query: listTemplates  
+  });
+
+  let boardTemplates = response.data.listTemplates.items.map(item => {
+      return new BoardTemplate(
+        item.colorGrid,
+        item.numGrid,
+        item.creator,
+        item.timeCreated,
+        item.creator,
+        item.creationMessage
+    );
+  });
+
+  return boardTemplates;
+}
 function BoardSquare({square}) {
   return(
     <div
@@ -24,23 +38,18 @@ function BoardSquare({square}) {
 };
 
 export function CreateBoard({squares}) {
-  // maybe put the board square stuff all in this function so it does stuff? Or maybe remove that for modularity. 
-  // this could potentially help with rendering or it will do the exact opposite lol. Takes away a layer
-  // if we render the board always. 
+  var boardSizes = "";
+  for (let index = 0; index < Math.sqrt(squares.length); index++) {
+      boardSizes += "1fr ";
+  }
 
-    var boardSizes = "";
-    for ( let index = 0; index < Math.sqrt(squares.length); index++) {
-      boardSizes = boardSizes + "1fr ";
-    }
-
-    return(
-      <div id="displayTemplate" style={{gridTemplateColumns: boardSizes}}>
-        {squares.map(square => (
-          <BoardSquare square={square}/>
-        ))}
-      </div>
-    );
-  
+  return (
+    <div id="displayTemplate" style={{gridTemplateColumns: boardSizes}}>
+      {squares.map((square, index) => (
+        <BoardSquare key={index} square={square} />  // Using index as key
+      ))}
+    </div>
+  );
 }
 
 // function to get the AWS time
@@ -70,48 +79,6 @@ export async function getSpecificTemplate(templateId){
   console.log(myTemplate.data.getTemplates);  
   return myTemplate.data.getTemplates;
 }
-
-
-// used apart of Home right now, can be modified for broader use
-export async function queryTemplates(){
-  
-  let multipleSquares = [];
-
-  const allTemplates = await client.graphql({
-    query: listTemplates  
-  });
-
-  let numGrid = allTemplates.data.listTemplates.items[0].numGrid
-  let colorGrid = allTemplates.data.listTemplates.items[0].colorGrid
-
-  let squares = numGrid.map(index => (
-    {color: colorGrid[index]}
-  ))
-
-  multipleSquares.push(squares);
-
-  numGrid = allTemplates.data.listTemplates.items[1].numGrid
-  colorGrid = allTemplates.data.listTemplates.items[1].colorGrid
-
-  squares = numGrid.map(index => (
-    {color: colorGrid[index]}
-  ))
-
-  multipleSquares.push(squares);
-
-  numGrid = allTemplates.data.listTemplates.items[3].numGrid
-  colorGrid = allTemplates.data.listTemplates.items[3].colorGrid
-
-  squares = numGrid.map(index => (
-    {color: colorGrid[index]}
-  ))
-
-  multipleSquares.push(squares);
-
-
-  return multipleSquares;
-}
-
 
 export async function submit(numGrid, colorGrid, tempProps){
   return await client.graphql({
---
+++ multipainter/src/utilities/TemplateClass.js
@@ -0,0 +1,19 @@
+
+export class boardSettings {
+    constructor(boardSize, dragSetting) {
+        this.boardSize = boardSize;
+        this.dragSetting = dragSetting;
+    }
+}
+
+export class BoardTemplate {
+    constructor(colorGrid, numberGrid, author, date, title, creationMessage, tags) {
+        this.colorGrid = colorGrid;
+        this.numberGrid = numberGrid;
+        this.author = author;
+        this.date = date;
+        this.title = title;
+        this.creationMessage = creationMessage;
+        this.tags = tags;
+    }
+}
